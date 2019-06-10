bd2b861c032870e0b0baa99085361732ac15ae42
bmohanty <bmohanty@mbidhul-01.ad.bakkt.net>
2019-06-10 07:57:38 +0000

    Second

---
+++ TestProject/src/Test_git.java
@@ -0,0 +1,13 @@
+
+public class Test_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\bmohanty\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
